f4d518773688c7a7084e640fc6c21c6e833d5024
ST LEON <leonst998@gmail.com>
2015-06-29 18:25:35 +0300

    карты
    на главной, при добавлении граффити

---
+++ graffities/static/js/add_map.js
@@ -0,0 +1,48 @@
+ymaps.ready(init);
+var myMap;
+
+function init () {
+    myMap = new ymaps.Map("map", {
+        center: [55.753559, 37.609218],
+        zoom: 11
+    }, {
+        balloonMaxWidth: 200,
+        searchControlProvider: 'yandex#search'
+    });
+
+    // Обработка события, возникающего при щелчке
+    // левой кнопкой мыши в любой точке карты.
+    // При возникновении такого события откроем балун.
+    myMap.events.add('click', function (e) {
+        if (!myMap.balloon.isOpen()) {
+            var coords = e.get('coords');
+            $('#id_lat').val(coords[0].toPrecision(8));
+            $('#id_lon').val(coords[1].toPrecision(8));
+            myMap.balloon.open(coords, {
+                contentHeader:'Ура!',
+                contentBody:'<p>Вы отметили граффити на карте.</p>' +
+                    '<p>Координаты граффити: ' + [
+                    coords[0].toPrecision(8),
+                    coords[1].toPrecision(8)
+                    ].join(', ') + '</p>',
+                contentFooter:'<sup>Мы обязательно проверим :)</sup>'
+            });
+        }
+        else {
+            myMap.balloon.close();
+        }
+    });
+
+    // Обработка события, возникающего при щелчке
+    // правой кнопки мыши в любой точке карты.
+    // При возникновении такого события покажем всплывающую подсказку
+    // в точке щелчка.
+    myMap.events.add('contextmenu', function (e) {
+        myMap.hint.open(e.get('coords'), 'Щелкните левой кнопкой');
+    });
+
+    // Скрываем хинт при открытии балуна.
+    myMap.events.add('balloonopen', function (e) {
+        myMap.hint.close();
+    });
+}
---
+++ graffities/static/js/main_map.js
@@ -0,0 +1,51 @@
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [55.76, 37.64],
+            zoom: 12
+        }),
+        objectManager = new ymaps.ObjectManager({
+            // Чтобы метки начали кластеризоваться, выставляем опцию.
+            clusterize: true,
+            // ObjectManager принимает те же опции, что и кластеризатор.
+            gridSize: 32
+        });
+
+    // Чтобы задать опции одиночным объектам и кластерам,
+    // обратимся к дочерним коллекциям ObjectManager.
+    objectManager.objects.options.set('preset', 'islands#redDotIcon');
+    objectManager.clusters.options.set('preset', 'islands#redClusterIcons');
+    myMap.geoObjects.add(objectManager);
+
+    $.ajax({
+        url: "/api/graffities"
+    }).done(function(data) {
+        // полученные данные приведем к требуемой структуре
+        // https://tech.yandex.ru/maps/doc/jsapi/2.1/ref/reference/ObjectManager-docpage/#add
+        // проблема в том, что при больших данных надо будет использовать pagination
+        var add_data = {
+          "type": "FeatureCollection",
+          "features": []
+        }
+        for (i = 0; i < data.length; i++) {
+          add_data["features"].push(
+            {
+              "type": "Feature",
+              "id": i,
+              "geometry": {
+                "type": "Point",
+                "coordinates": [data[i]["lat"], data[i]["lon"]]
+                },
+              "properties": {
+                "balloonContentHeader": data[i]["name"],
+                "balloonContent": '<center><a href="/graffiti/'+ data[i]["id"] +'" target=_blank><img class="img-responsive" src="'+ data[i]["photo"] +'" width="250px" height="250px"></a></center>',
+                "balloonContentFooter": '<center><a href="/graffiti/'+ data[i]["id"] +'" target=_blank>Посмотреть и обсудить</a></center>',
+                "clusterCaption": "Граффити" + data[i]["id"],
+                "hintContent": data[i]["name"]
+              }
+            }
+          );
+        }
+        objectManager.add(add_data);
+    });
+
+});

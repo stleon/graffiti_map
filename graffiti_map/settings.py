0a94397fb3605c7c266493e80846c914c6c126a2
ST LEON <leonst998@gmail.com>
2015-07-07 15:09:10 +0300

    headers & code to add on site

--- graffiti_map/settings.py
+++ graffiti_map/settings.py
@@ -200,6 +200,16 @@ ADMIN_URL = get_env_setting('ADMIN_URL')
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_URLS_REGEX = r'^/api/.*$'
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = (
+        'x-requested-with',
+        'content-type',
+        'accept',
+        'origin',
+        'authorization',
+        'x-csrftoken',
+        'dnt',
+        'accept-encoding',
+    )
 
 # THUMBNAIL
 

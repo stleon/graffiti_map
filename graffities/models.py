42c4d18c58b8f7f6a508a7631158b1b78c568796
ST LEON <leonst998@gmail.com>
2015-07-07 12:20:55 +0300

    resize image, validator and post-delete signal

--- graffities/models.py
+++ graffities/models.py
@@ -2,22 +2,34 @@ import os
 import uuid
 from django.db import models
 from django.core.urlresolvers import reverse
-from sorl.thumbnail import ImageField, get_thumbnail, delete
+from sorl.thumbnail import ImageField, delete
 from django.core.files.base import ContentFile
+from django_resized import ResizedImageField
+from django.core.exceptions import ValidationError
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
 
 
 def get_file_path(instance, filename):
     filename = "%s.%s" % (uuid.uuid4(), filename.split('.')[-1].lower())
-    return os.path.join('users_img/', filename)
+    return filename
+
+
+def validate_image(image):
+    "Валидатор, проверяем формат файла"
+    if not str(image).lower().endswith(('.jpg', '.jpeg', )):
+        raise ValidationError('Поддерживается только jpg!')
 
 
 class Graffiti(models.Model):
     width = models.PositiveIntegerField(editable=False, )
     height = models.PositiveIntegerField(editable=False, )
-    photo = models.ImageField('Фото',
+    photo = ResizedImageField('Фото',
+                              size=[2500, 1024],
                               upload_to=get_file_path,
                               height_field='height',
-                              width_field='width')
+                              width_field='width',
+                              validators=[validate_image])
     name = models.CharField('Название', max_length=50)
     comment = models.CharField('Комментарий', max_length=140)
     lat = models.FloatField('Географическая широта', )
@@ -38,25 +50,14 @@ class Graffiti(models.Model):
     def get_absolute_url(self):
         return reverse('graffiti', kwargs={'pk': self.id, })
 
-    def save(self, *args, **kwargs):
-        # Можно и асинхронную очередь сделать, но на первое время норм
-        if not self.id:
-            # Сначала сохраняем картинку
-            super(Graffiti, self).save(*args, **kwargs)
-            # Изменяем картинку
-            resized = get_thumbnail(self.photo, 'x1024',
-                                    quality=99,
-                                    format='JPEG')
-            # сохраняем
-            self.photo.save(resized.name, ContentFile(resized.read()), True)
-        super(Graffiti, self).save(*args, **kwargs)
-
-    def delete(self, *args, **kwargs):
-        storage, path = self.photo.storage, self.photo.path
-        super(Graffiti, self).delete(*args, **kwargs)
-        storage.delete(path)  # Удаляем файл после модели
-        delete(path)
-
     class Meta:
         verbose_name = 'Граффити'
         verbose_name_plural = verbose_name
+
+
+@receiver(post_delete, sender=Graffiti)
+def photo_post_delete_handler(sender, **kwargs):
+    graffiti = kwargs['instance']
+    storage, path = graffiti.photo.storage, graffiti.photo.path
+    storage.delete(path)
+    delete(path)
